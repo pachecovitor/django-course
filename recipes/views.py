e34f3e10d4cbad10240c3fb8c6b9c4bdfc3c59ee
Vitor Pacheco <vitorpacheco2000@hotmail.com>
2024-02-23 21:46:11 -0300

    commit all

---
+++ recipes/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return render(request, 'recipes/pages/home.html', status=201)
+
+# Create your views here.

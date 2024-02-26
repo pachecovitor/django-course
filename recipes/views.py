5d17f747b6ebf70f10780597157b5c9636daf825
pachecovitor <vitorpacheco2000@hotmail.com>
2024-02-26 18:47:24 -0300

    commit all

--- recipes/views.py
+++ recipes/views.py
@@ -4,4 +4,6 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'recipes/pages/home.html', status=201)
 
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html', status=201)
 # Create your views here.
